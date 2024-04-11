b34eb30cdd8cc21d678876d34142c037d84d0459
Lehuanahuel <162445079+nabudev@users.noreply.github.com>
2024-04-11 01:23:47 -0300

    Update elif.py

--- condicionales/elif.py
+++ condicionales/elif.py
@@ -1,7 +1,7 @@
 ingreso_mensual = 81000
 gasto_mensual = 80000
 
-#if anidados y else if (elif)
+#if anidados y else if (elif) pa
 
 if ingreso_mensual > 10000:
     if ingreso_mensual - gasto_mensual < 0: 
